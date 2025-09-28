c0bb9ea132162dcd7ea944c88d8081e2dfc18171
Artem Kurochka <artem.04k@mail.ru>
2025-09-28 17:15:59 +0700

    Resolve CORS issues. Add .gitignore

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 from contextlib import asynccontextmanager
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.backend.db import init_db, engine
 from app.routers import upload
 
@@ -14,5 +15,20 @@ async def lifespan(app: FastAPI):
 
 
 app = FastAPI(title="UploadDocsService", lifespan=lifespan)
+
+origins = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+]
+# CORS middleware ДО роутеров!
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(upload.router, prefix="/api")
--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,1 +1,4 @@
+from .document import Document
+from .chunk import DocumentChunk
 
+__all__ = ['Document', 'DocumentChunk']
